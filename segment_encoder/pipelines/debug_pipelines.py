84e22980f0ab6d51c601099f47e0d71efff16f06
Roman Zinovyev <roman__z@mail.ru>
2019-03-18 16:50:46 +0300

    debug

--- segment_encoder/pipelines/debug_pipelines.py
+++ segment_encoder/pipelines/debug_pipelines.py
@@ -4,12 +4,28 @@ after data.load
 first n_samples/100 were taken and saved as csv, npy
 '''
 import matplotlib.pyplot as plt
-from segment_encoder.tools.segment_tools import *
-from segment_encoder.tools.voxel_tools import *
-
-from segment_encoder.tools.plot_tools import plot_segment_pcl, plot_segments_pcl
+import numpy as np
 import pickle
 import os
+
+import tensorflow as tf
+HDF5_PATH = r'../../notebooks/'
+f_name = 'data18.hdf5'
+print(os.path.join(HDF5_PATH, f_name))
+
+x_data = tf.keras.utils.HDF5Matrix(HDF5_PATH, 'data')
+
+from segment_encoder.tools.segment_tools import RandomRemover,\
+                                                PlaneRemover,\
+                                                EigenAligner,\
+                                                RandomRotator,\
+                                                CoordinatesRescaler
+from segment_encoder.tools.voxel_tools import Voxelizer
+
+from segment_encoder.tools.plot_tools import plot_segment_pcl, \
+                                             plot_segments_pcl, \
+                                             plot_segment_voxelbox
+
 
 SEGMENT_ID = 9 # which segment to show
 
@@ -19,7 +35,7 @@ SMALL_DATA_DIR = r'../small_data'
 
 
 with open(os.path.join(SMALL_DATA_DIR, 'segments.csv'), "rb") as f:
-    segments_pointcloud = pickle.load(f)
+    pointcloud_segments= pickle.load(f)
 
 positions = np.load(os.path.join(SMALL_DATA_DIR, 'positions.npy'))
 classes = np.load(os.path.join(SMALL_DATA_DIR, 'classes.npy'))
@@ -27,56 +43,56 @@ features = np.load(os.path.join(SMALL_DATA_DIR, 'features.npy'))
 duplicate_classes = np.load(os.path.join(SMALL_DATA_DIR, 'duplicate_classes.npy'))
 n_classes = classes.shape[0]
 
-segments_pointcloud = segments_pointcloud[:10]
+pointcloud_segments = pointcloud_segments[:10]
 
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
 
-segments_pointcloud = RandomRemover().fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
+pointcloud_segments = RandomRemover().fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
 
-segments_pointcloud = PlaneRemover().fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
+pointcloud_segments = PlaneRemover().fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
 
-segments_pointcloud = EigenAligner().fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
+pointcloud_segments = EigenAligner().fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
 
-segments_pointcloud = RandomRotator().fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
-
-coordinates_scaler = CoordinatesRescaler(scale_method = "fixed",
-                    center_method = "mean",
-                    scale = np.array((8,8,4)),
-                    voxel_shape = np.array((32, 32, 16)),
-                    min_scale_threshold = np.array((3.2, 3.2, 1.6)))
-
-segments_pointcloud = coordinates_scaler.fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
-
-
-segments_voxelbox = Voxelizer().fit_transform(segments_pointcloud)
-segment_voxelbox = segments_voxelbox[SEGMENT_ID]
-print(segment_voxelbox.shape)
-
-
-segments_pointcloud = EigenAligner().fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
+pointcloud_segments = RandomRotator().fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
 
 coordinates_scaler = CoordinatesRescaler(scale_method = "fixed",
                     center_method = "mean",
                     scale = np.array((8,8,4)),
                     voxel_shape = np.array((32, 32, 16)),
                     min_scale_threshold = np.array((3.2, 3.2, 1.6)))
-segments_pointcloud = coordinates_scaler.fit_transform(segments_pointcloud)
-segment_pointcloud = segments_pointcloud[SEGMENT_ID]
-print(segment_pointcloud.shape)
 
-segments_voxelbox = Voxelizer().fit_transform(segments_pointcloud)
-segment_voxelbox = segments_voxelbox[SEGMENT_ID]
-print(segment_voxelbox.shape)
+pointcloud_segments = coordinates_scaler.fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
+
+
+voxelbox_segments = Voxelizer().fit_transform(pointcloud_segments)
+voxelbox_segment = voxelbox_segments[SEGMENT_ID]
+print(voxelbox_segment.shape)
+
+
+pointcloud_segments = EigenAligner().fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
+
+coordinates_scaler = CoordinatesRescaler(scale_method = "fixed",
+                    center_method = "mean",
+                    scale = np.array((8,8,4)),
+                    voxel_shape = np.array((32, 32, 16)),
+                    min_scale_threshold = np.array((3.2, 3.2, 1.6)))
+pointcloud_segments = coordinates_scaler.fit_transform(pointcloud_segments)
+pointcloud_segment = pointcloud_segments[SEGMENT_ID]
+print(pointcloud_segment.shape)
+
+voxelbox_segments = Voxelizer(voxel_shape = np.array((64, 64, 8))).fit_transform(pointcloud_segments)
+voxelbox_segment = voxelbox_segments[SEGMENT_ID]
+print(voxelbox_segment.shape)
