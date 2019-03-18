ffe75d21637bfd294ae9593153a8f491a4ac0b0b
Roman Zinovyev <roman__z@mail.ru>
2019-03-18 16:51:39 +0300

    main

--- segment_encoder/pipelines/main_pipelines.py
+++ segment_encoder/pipelines/main_pipelines.py
@@ -1,55 +1,55 @@
+# todo maybe use global_ids, object_ids
+
 from sklearn.pipeline import Pipeline
-from ..tools.segment_tools import *
+from segment_encoder.tools.segment_tools import RandomRemover,\
+                                                PlaneRemover,\
+                                                EigenAligner,\
+                                                RandomRotator,\
+                                                CoordinatesRescaler
+from segment_encoder.tools.voxel_tools import Voxelizer
+
 import pickle
 import os
+import numpy as np
 
 SMALL_DATA_DIR = r'../small_data'
 
-with open(os.path.join(SMALL_DATA_DIR, 'segments.csv'), "rb") as f:   # Unpickling
-    segments = pickle.load(f)
 
-positions = np.load(os.path.join(SMALL_DATA_DIR, 'positions.npy'))
-classes = np.load(os.path.join(SMALL_DATA_DIR, 'classes.npy'))
-features = np.load(os.path.join(SMALL_DATA_DIR, 'features.npy'))
-duplicate_classes = np.load(os.path.join(SMALL_DATA_DIR, 'duplicate_classes.npy'))
-n_classes = classes.shape[0]
+def train_pipeline(pointcloud_segments):
 
-
-def init_pipeline(segments=None, classes=None, positions=None, train_ids=None, scaler_path=None):
-    init_pipeline = Pipeline([
-            ('RobotAligner', RobotAligner(positions)),
-        ])
-    return init_pipeline.fit_transform(segments)
-
-
-def train_pipeline(segments, train_ids, to_voxelise = True):
-
-    segments_preprocessing_pipeline = Pipeline([
+    augmentation_pipeline = Pipeline\
+            ([
             ('RandomRemover', RandomRemover()),
             ('PlaneRemover', PlaneRemover()),
-            ('EigenAligner', EigenAligner(method='eigen')),
+            ('EigenAligner', EigenAligner()),
             ('Rotator', RandomRotator()),
     ])
 
-    segments = segments_preprocessing_pipeline.fit_transform(segments)
+    pointcloud_segments = augmentation_pipeline.fit_transform(pointcloud_segments)
 
-    if to_voxelise:
-        voxels_preprocessing_pipeline = Pipeline([
-                ('CoordinatesRescaler', CoordinatesRescaler(scale_method = "fixed", center_method = "mean")),
-                ('Jitter', Jitter()),
-                ('Voxelizer', Voxelizer()),
-                ('VoxelNormalizer', VoxelNormalizer(train_ids=train_ids))
-            ])
+    preprocessing_pipeline = Pipeline([
+            ('CoordinatesRescaler', CoordinatesRescaler(scale_method = "fit", center_method = "mean")),
+            # ('Jitter', Jitter()),
+            ('Voxelizer', Voxelizer()),
+            # ('VoxelNormalizer', VoxelNormalizer(train_ids=train_ids))
+        ])
 
-        voxels = voxels_preprocessing_pipeline.fit_transform(segments)
+    voxelbox = preprocessing_pipeline.fit_transform(pointcloud_segments)
 
-    return voxels
+    return voxelbox
 
 def load_segments():
-    raise NotImplementedError
-    return
+    with open(os.path.join(SMALL_DATA_DIR, 'segments.csv'), "rb") as f:  # Unpickling
+        segments = pickle.load(f)
+
+    positions = np.load(os.path.join(SMALL_DATA_DIR, 'positions.npy'))
+    classes = np.load(os.path.join(SMALL_DATA_DIR, 'classes.npy'))
+    features = np.load(os.path.join(SMALL_DATA_DIR, 'features.npy'))
+    duplicate_classes = np.load(os.path.join(SMALL_DATA_DIR, 'duplicate_classes.npy'))
+    n_classes = classes.shape[0]
+    return segments, positions, classes, features, duplicate_classes, n_classes
 
 if __name__ == '__main__':
-    segments = load_segments()
-    segments = init_pipeline(segments)
-    segments = train_pipeline(segments)
+    pointcloud_segments, positions, classes, features, duplicate_classes, n_classes = load_segments()
+    voxelbox_segments = train_pipeline(pointcloud_segments)
+    print(voxelbox_segments.shape)
